03207be8b8490d3e3d234276566883a80929665b
Delenko <34238377+delenko@users.noreply.github.com>
2018-10-20 11:21:45 -0700

    Update Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -1,9 +1,37 @@
-#pragma once
+#include "Item.hpp"
 
-class Item {
-public:
-	Item();
+#include <iostream>
+#include <string>
 
-private:
 
-};
+
+
+	Item::Item() {
+
+	};
+
+	//Establish weapons
+	struct weapons {
+		std::string name;
+		int quantity;
+		int damage;
+		//bonus effects
+	};
+	//establish armors
+	struct armors {
+		std::string name;
+		int quantity;
+		int defense;
+	};
+	//establish potions
+	struct potions {
+		std::string name;
+		int quantity;
+		int health;
+	};
+
+
+
+	void Item::returnItemEffect(Item& item) {
+
+	}
